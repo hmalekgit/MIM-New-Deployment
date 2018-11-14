13d91f0586fe7e2632004acdb8e56ab297552dcb
hmalekgit <45003040+hmalekgit@users.noreply.github.com>
2018-11-14 12:06:02 +0200

    Add files via upload

---
+++ devops.ps1
@@ -0,0 +1,7 @@
+﻿Import-Module LithnetRMA
+$obj = New-Resource -ObjectType Person
+$obj.AccountName = "DevOps"
+$obj.Domain = "lab"
+$obj.DisplayName = "Azure DevOps"
+$obj.JobTitles = @("Test1", "Test2")
+Save-Resource $obj
